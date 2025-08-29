a159c35f698a5db970e4e5ccb0928418e3ddb5ed
yoru <jordanchandra1@gmail.com>
2025-08-30 03:10:20 +0700

    feat: add simple hand landmark

--- hand.py
+++ hand.py
@@ -16,11 +16,19 @@ while True:
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
 
+    if results.multi_hand_landmarks:
+        for hand in results.multi_hand_landmarks:
+            for id, lm in enumerate(hand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+
+            mpDraw.draw_landmarks(img, hand, mpHands.HAND_CONNECTIONS)
+
     currTime = time.time()
     fps = 1 / (currTime - prevTime)
     prevTime = currTime
 
-    cv2.putText(img, str(int(fps)), (10, 20), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 1)
+    cv2.putText(img, str(int(fps)), (10, 35), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 1)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
